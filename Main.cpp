0bf1b058ab95f13a04898dc7c2228a0e7ac80407
Murtaza18 <murtazatuta72@gmail.com>
2023-04-16 02:12:51 +0530

    Add Display time slots

--- Main.cpp
+++ Main.cpp
@@ -1,42 +1,55 @@
 #include <fstream>
 #include <iostream>
 #include <limits>
+#include <string>
+
+#define MOVIE_NUM 3
 
 using namespace std;
 
 class Movie
 {
-public:
+private:
     int id;
     string name;
     int duration;
     float rating;
+
+    friend class Theater;
+    friend istream& operator>>(istream& file, Movie& m);
 };
 
-// istream& operator>>(istream& ins, Movie& m)
-// {
-//     getline(ins, m.name);
-//     getline(ins, m.duration);
-//     getline(ins, m.rating);
-//
-//     // skips blank line
-//     ins.ignore(numeric_limits<streamsize>::max(), '\n');
-//
-//     return ins;
-// }
+istream& operator>>(istream& file, Movie& m)
+{
+    string temp;
+
+    getline(file, m.name);
+
+    getline(file, temp);
+    m.duration = stoi(temp);
+
+    getline(file, temp);
+    m.rating = stof(temp);
+
+    // skips blank line
+    file.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    return file;
+}
 
 class Theater
 {
 private:
     string name;
     string location;
-    int timings[5] = {9, 12, 15, 18, 21};
-    Movie movies[3];
+    int timeSlot[6] = {9, 12, 15, 18, 21, 0};
+    Movie movies[MOVIE_NUM];
 
 public:
     Theater();
     int DisplayMovies();
     void DisplayMovieDetails(int id);
+    int DisplayTimeSlots();
 };
 
 Theater::Theater()
@@ -47,10 +60,7 @@ Theater::Theater()
     while (!movieFile.eof())
     {
         movies[i].id = i + 1;
-        movieFile >> movies[i].name;
-        movieFile >> movies[i].duration;
-        movieFile >> movies[i].rating;
-
+        movieFile >> movies[i];
         i++;
     }
     movieFile.close();
@@ -63,25 +73,59 @@ int Theater::DisplayMovies()
 
     cout << endl;
     cout << "Select a Movie" << endl;
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < MOVIE_NUM; i++)
     {
         cout << "\t[" << i + 1 << "] " << movies[i].name << endl;
     }
 
     cout << "Movie number: ";
     cin >> choice;
+
+    DisplayTimeSlots();
     DisplayMovieDetails(choice);
+
     return choice;
 }
 
 void Theater::DisplayMovieDetails(int id)
 {
     int index = id - 1;
+    try
+    {
+        if (index < MOVIE_NUM && index >= 0)
+        {
+            cout << endl;
+            cout << "Movie Name:\t" << movies[index].name << endl;
+            cout << "Duration:\t" << movies[index].duration << " minutes"
+                 << endl;
+            cout << "Ratings:\t" << movies[index].rating << endl;
+        }
+        else
+        {
+            throw index;
+        }
+    }
+    catch (int index)
+    {
+        cout << "Invalid choice" << endl;
+    }
+}
+
+int Theater::DisplayTimeSlots()
+{
+    int choice;
 
     cout << endl;
-    cout << "Movie Name:\t" << movies[index].name << endl;
-    cout << "Duration:\t" << movies[index].duration << " minutes" << endl;
-    cout << "Ratings:\t" << movies[index].rating << endl;
+    cout << "Select a time slot" << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "\t[" << i + 1 << "] " << timeSlot[i] << ":00 to "
+             << timeSlot[i + 1] << ":00" << endl;
+    }
+    cout << "Slot number: ";
+    cin >> choice;
+
+    return choice;
 }
 
 class Ticket
